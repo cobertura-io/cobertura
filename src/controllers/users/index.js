2b299db1c121d3cbd62ce9dc1a2cd335bdffeab0
Waldrey <waldreysouzasilva@outlook.com>
2019-06-10 19:27:04 -0300

    profile base

--- src/controllers/users/index.js
+++ src/controllers/users/index.js
@@ -4,10 +4,19 @@ module.exports = {
   async getById(req, res) {
     const id = req.params.id
 
-    const response = await api.get(`users/${id}`)
-      .then(result => result.data )
+    const user = await api.get(`users/broker/${id}`)
+      .then(result => result.data.data[0] )
+      .catch(err => console.log(err))
+    
+    const houses = await api.get(`houses/${id}`)
+      .then(result => result.data.data )
       .catch(err => console.log(err))
 
-    return res.render('profile', { layout: 'default', user: response.data });
+    return res.render('profile', { 
+      layout: 'default', 
+      user, 
+      houses,
+      title: `Cobertura: ${user.name}` 
+    });
   }
 }
