99b0f6654c9ed3da02e7c06f0bac27112a8add6f
Deniel <deniel_16@live.com>
2019-06-23 15:59:39 -0300

    corrige scrollbar

--- src/routes.js
+++ src/routes.js
@@ -34,7 +34,7 @@ routes.get('/imoveis', (req, res) => {
       id: 0,
       title: 'Uma casa incrível',
       picture: 'https://images.pexels.com/photos/2287310/pexels-photo-2287310.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=2&h=650&w=940',
-      address: 'Rua Gisele Martins, Cidade Morumbi',
+      address: 'Cidade Morumbi',
       price: '570.000,00',
       bedroom: 3,
       bathroom: 4,
@@ -42,11 +42,12 @@ routes.get('/imoveis', (req, res) => {
       size: 500,
       lat: -23.2530996,
       lng: -45.898194100000005
-    }, {
+    }, 
+    {
       id: 1,
       title: 'Uma casa muito doida',
       picture: 'https://images.unsplash.com/photo-1475855581690-80accde3ae2b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
-      address: 'Avenida Salmão, Aquarius',
+      address: 'Aquarius',
       price: '1.000.000,00',
       bedroom: 5,
       bathroom: 5,
@@ -54,7 +55,60 @@ routes.get('/imoveis', (req, res) => {
       size: 1300,
       lat: -23.219373,
       lng: -45.907630
-    }],
+    },
+    {
+      id: 2,
+      title: 'Uma casa muito doida',
+      picture: 'https://images.unsplash.com/photo-1475855581690-80accde3ae2b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+      address: 'Aquarius',
+      price: '1.000.000,00',
+      bedroom: 5,
+      bathroom: 5,
+      garage: 4,
+      size: 1300,
+      lat: -23.219373,
+      lng: -45.907630
+    },
+    {
+      id: 3,
+      title: 'Uma casa muito doida',
+      picture: 'https://images.unsplash.com/photo-1475855581690-80accde3ae2b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+      address: 'Aquarius',
+      price: '1.000.000,00',
+      bedroom: 5,
+      bathroom: 5,
+      garage: 4,
+      size: 1300,
+      lat: -23.219373,
+      lng: -45.907630
+    },
+    {
+      id: 4,
+      title: 'Uma casa muito doida',
+      picture: 'https://images.unsplash.com/photo-1475855581690-80accde3ae2b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+      address: 'Aquarius',
+      price: '1.000.000,00',
+      bedroom: 5,
+      bathroom: 5,
+      garage: 4,
+      size: 1300,
+      lat: -23.219373,
+      lng: -45.907630
+    },
+    {
+      id: 5,
+      title: 'Uma casa muito doida',
+      picture: 'https://images.unsplash.com/photo-1475855581690-80accde3ae2b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+      address: 'Aquarius',
+      price: '1.000.000,00',
+      bedroom: 5,
+      bathroom: 5,
+      garage: 4,
+      size: 1300,
+      lat: -23.219373,
+      lng: -45.907630
+    },
+  ],
     user: req.session.user 
   })
 })
